bba0182cd440665064f392a0025e6e733ddcea94
Sah!l <sahil.maji@gmail.com>
2024-10-28 11:32:43 +0530

    add css

--- layouts/Spam.js
+++ layouts/Spam.js
@@ -1,68 +1,8 @@
 "use client";
 import axios from "axios";
 import { useState } from "react";
+import "./spam.css";
 
-import Link from "next/link";
-
-function Component() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center space-y-4 border-black py-12 md:py-24">
-      <div className="flex flex-col items-center justify-center gap-10 space-y-2">
-        <div className="flex flex-col items-center justify-center space-y-2">
-          <CircleCheckIcon className="h-12 w-12 text-gray-500 dark:text-gray-400" />
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-            Payment successful
-          </h1>
-        </div>
-
-        <p className="max-w-[600px] text-center text-gray-500 dark:text-gray-400 md:text-xl/relaxed">
-          Your order has been confirmed and is now being processed. Thank you
-          for shopping with us.
-        </p>
-      </div>
-    </div>
-  );
-}
-
-function CircleCheckIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="m9 12 2 2 4-4" />
-    </svg>
-  );
-}
-
-function UserIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-      <circle cx="12" cy="7" r="4" />
-    </svg>
-  );
-}
 const Spam = () => {
   // 161304,-3.04354,-4.147345,1.621898,0.702232,1.509553,-1.752889,0.853788,-0.408466,-0.974442,0.395108,-1.666658,2.393264,-2.684519,1.225875,-1.075812,0.694202,1.795492,-2.140392,0.642101,-0.894303,0.284294,-1.052755,1.023842,-1.345432,0.932821,0.194746,-0.495807,0.122782,325.5
   const [input, setInput] = useState([
@@ -119,9 +59,33 @@ const Spam = () => {
           Verify
         </button>
       </div>
-      <div>
-        {message}
-        <Component />
+      <div>{message}</div>
+      <div class="text-center">
+        <a href="#myModal" class="trigger-btn" data-toggle="modal">
+          Click to Open Confirm Modal
+        </a>
+      </div>
+      <div id="myModal" class="modal fade">
+        <div class="modal-dialog modal-confirm">
+          <div class="modal-content">
+            <div class="modal-header">
+              <div class="icon-box">
+                <i class="material-icons">&#xE876;</i>
+              </div>
+              <h4 class="modal-title">Awesome!</h4>
+            </div>
+            <div class="modal-body">
+              <p class="text-center">
+                Your booking has been confirmed. Check your email for detials.
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-success btn-block" data-dismiss="modal">
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
